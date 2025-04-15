282908d2878570484c219fdc83c8140dd15bd970
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2025-04-15 15:38:43 +0700

    feat: serve logo

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
 
 	server := &http.Server{
 		Addr:    ":8080",
