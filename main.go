68f827c1ecb37d393389a8df25dbdd2ff4210bd2
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2025-04-15 16:35:47 +0700

    refactor(api): move /metrics and /reset to /admin namespace

--- main.go
+++ main.go
@@ -18,9 +18,9 @@ func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 	mux.Handle("/assets", http.StripPrefix("/assets", http.FileServer(http.Dir("./assets"))))
-	mux.HandleFunc("GET /healthz", handlerReadiness)
-	mux.HandleFunc("GET /metrics", apiCfg.handlerMetrics)
-	mux.HandleFunc("POST /reset", apiCfg.handlerReset)
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 
 	server := &http.Server{
 		Addr:    ":8080",
--- metrics.go
+++ metrics.go
@@ -17,11 +17,10 @@ func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(fmt.Sprintf(`
 <html>
-
-<body>
-	<p>Hits: %d</p>
-</body>
-
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
 </html>
 	`, cfg.fileserverHits.Load())))
 }
