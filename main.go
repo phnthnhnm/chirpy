0f09109cc839c03b0a7e30f7b668386411a335ec
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2025-04-15 17:40:54 +0700

    feat(api): add post /api/users endpoint

--- main.go
+++ main.go
@@ -42,6 +42,7 @@ func main() {
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 	mux.HandleFunc("POST /api/validate_chirp", apiCfg.handlerValidateChirp)
+	mux.HandleFunc("POST /api/users", apiCfg.handlerUsersCreate)
 
 	server := &http.Server{
 		Addr:    ":8080",
