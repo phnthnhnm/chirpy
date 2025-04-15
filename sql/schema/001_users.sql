aea3b234f00778338e6b9be946eb158a69548a5a
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2025-04-15 19:07:56 +0700

    feat(api): post /api/users endpoint now require passwords (hashed)

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -1,9 +1,9 @@
 -- +goose Up
 CREATE TABLE users (
-   id UUID PRIMARY KEY,
-   created_at TIMESTAMP NOT NULL,
-   updated_at TIMESTAMP NOT NULL,
-   email TEXT NOT NULL UNIQUE
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    email TEXT NOT NULL UNIQUE
 );
 
 -- +goose Down
